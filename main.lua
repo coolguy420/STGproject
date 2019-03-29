eb63470f4b0bdc3f8119980db5237424e51e442c
coolguy420 <ono.alexander@yahoo.co.uk>
2019-03-29 00:23:01 +0000

    Add files via upload

--- main.lua
+++ main.lua
@@ -15,6 +15,14 @@ bullets = {}
 
 enemies = {}
 
+animations = {}
+
+waves = {}
+
+numEnemies = 0
+
+allEnemiesSpawned = 0
+
 isAlive = true
 score = 0
 
@@ -32,6 +40,8 @@ function love.load(arg)
 		imgR = nil,
 		box = {x = 195, y = 595, h = 10, w = 10}
 	}
+	currentWave = 1
+	
 	-- Load images for player/enemies/bullets
 		player.img = love.graphics.newImage('assets/ship.png')
 		player.imgL = love.graphics.newImage('assets/shipleft.png')
@@ -40,27 +50,80 @@ function love.load(arg)
 		thinBulletImg = love.graphics.newImage('assets/bullet2.png')
 		enemyImg = love.graphics.newImage('assets/enemy1.png')
 		shoot = love.audio.newSource("shoot.mp3", "static")
+		smallExplosion = love.graphics.newImage("assets/exp4.png")
+
+	wave1 = {enemyList = {},
+			 time = 0, maxEnemies = 4}
+			
+	table.insert(wave1.enemyList, {x = 200, y = -10, h = 38, w = 40, img = enemyImg, hp = 10, time = 50})
+	table.insert(wave1.enemyList, {x = 300, y = -10, h = 38, w = 40, img = enemyImg, hp = 10, time = 100})
+	table.insert(wave1.enemyList, {x = 400, y = -10, h = 38, w = 40, img = enemyImg, hp = 10, time = 150})
+	table.insert(wave1.enemyList, {x = 420, y = -10, h = 38, w = 40, img = enemyImg, hp = 10, time = 200})
+
+	table.insert(waves, wave1)
+
+	wave2 = {enemyList = {},
+			 time = 0, maxEnemies = 4}
+
+	table.insert(wave2.enemyList, {x = 200, y = -10, h = 38, w = 40, img = enemyImg, hp = 10, time = 100})
+	table.insert(wave2.enemyList, {x = 300, y = -10, h = 38, w = 40, img = enemyImg, hp = 10, time = 150})
+	table.insert(wave2.enemyList, {x = 400, y = -10, h = 38, w = 40, img = enemyImg, hp = 10, time = 200})
+	table.insert(wave2.enemyList, {x = 420, y = -10, h = 38, w = 40, img = enemyImg, hp = 10, time = 250})
+
+	table.insert(waves, wave2)
 end -- end love.load()
 
 function love.update(dt)
-	
 	-- for testing purposes until waves are implemented, spawn one enemy
 	-- then spawn nothing else
 
 	--createEnemyTimer = createEnemyTimer - (1 * dt)
 	--if createEnemyTimer < 0 then
-		if noEnemy then
-			enemy = new_single_box_enemy(200, -10, 38, 40, enemyImg)
-			table.insert(enemies, enemy)
-			noEnemy = false
-		end
+	--	if noEnemy then
+	--		enemy = new_enemy(200, -10, 38, 40, enemyImg, 10)
+	--		table.insert(enemies, enemy)
+	--		enemy = new_enemy(300, -10, 38, 40, enemyImg, 10)
+	--		table.insert(enemies, enemy)
+	--		enemy = new_enemy(100, -10, 38, 40, enemyImg, 10)
+	--		table.insert(enemies, enemy)
+	--		enemy = new_enemy(400, -10, 38, 40, enemyImg, 10)
+	--		table.insert(enemies, enemy)
+	--
+	--		noEnemy = false
+	--	end
 	--end
+	if allEnemiesSpawned == 1 then
+		if next(enemies) == nil then
+			currentWave = currentWave + 1
+			allEnemiesSpawned = 0
+		end
+	else
+		waves[currentWave].time = waves[currentWave].time + 1
+		for _, enemy in pairs(waves[currentWave].enemyList) do
+			if (enemy.time == waves[currentWave].time) then
+				newEnemy = new_enemy(enemy.x, enemy.y, enemy.h, enemy.w, enemy.img, enemy.hp)
+				table.insert(enemies, newEnemy)
+				numEnemies = numEnemies + 1
+				if numEnemies == waves[currentWave].maxEnemies then
+					allEnemiesSpawned = 1
+				end
+			end
+		end
+	end
 
 	-- is this creating the shooting bug?
 	canShootTimer = canShootTimer - (1 * dt)
 	if canShootTimer < 0 then
   		canShoot = true
 	end
+
+	for _, animation in pairs(animations) do
+		animation.currentTime = animation.currentTime + dt
+    	if animation.currentTime >= animation.duration then
+        	animation.currentTime = animation.currentTime - animation.duration
+    	end
+	end
+	
 
 	handleInput(dt)
 
@@ -96,15 +159,39 @@ function love.draw(dt)
 
 	drawEnemies()
 
+	drawValues() 
+
+	for i, animation in ipairs(animations) do
+		local spriteNum = math.floor(animation.currentTime / animation.duration * #animation.quads) + 1
+   		love.graphics.draw(animation.spriteSheet, animation.quads[spriteNum], animation.x, animation.y, 0, 4)
+    	if (spriteNum == animation.maxSprites) then
+    		table.remove(animations, i)
+    	end
+	end
+	
+
 end -- End love.draw()
 
-function new_single_box_enemy(initX, initY, height, width, initImg)
+--function new_wave(...)
+--	wave = {
+--		time = 0,
+--		enemies = {}
+--	}
+--	for i, enemy in ipairs(arg) do
+--		wave.enemies[i] = arg[i]
+--	end
+--	return wave
+--end -- end new_wave
+
+function new_enemy(initX, initY, height, width, initImg, HP)
 	-- Currently this function makes an enemy with only one hitbox variable in size
 	enemy = {
 		position = { x= initX, y= initY },
 		velocity = { x=0, y=100 }, -- Move down constantly
 		boxes = {},
-		img = initImg
+		img = initImg,
+		hp = HP,
+		hit = 0
 	}
 	enemy.boxes[1] = {x = initX, y = initY, h = height, w = width}
 	return enemy
@@ -271,8 +358,17 @@ function updateEnemies(Dt)
 			end
 			for b, bullet in ipairs(bullets) do
 				if is_colliding(bullet.box, box) then
-					table.remove(bullets, b)
-					table.remove(enemies, i)
+					-- this way of creating explosions is not scalable to enemies with multiple hitboxes or with a different size
+					-- TODO make it scalable
+					enemy.hit = 1
+					enemy.hp = enemy.hp - 1
+					if(enemy.hp < 0) then
+						animation = newAnimation(love.graphics.newImage("assets/exp4.png"), 24, 24, 1, enemy.position.x -20, enemy.position.y -20, 16)
+						table.remove(bullets, b)
+						table.remove(enemies, i)
+						table.insert(animations, animation)
+					end
+					
 				end
 			end
 		end
@@ -319,6 +415,10 @@ end -- end drawBullets
 
 function drawEnemies()
 	for _,enemy in pairs(enemies) do
+		if(enemy.hit == 1) then
+			love.graphics.setColor(0, 0, 50)
+			enemy.hit = 0
+		end
 		love.graphics.draw(enemy.img, enemy.position.x, enemy.position.y)
 		love.graphics.setColor(255, 0, 0)
 		for _, hitbox in pairs(enemy.boxes)do
@@ -327,3 +427,42 @@ function drawEnemies()
 		love.graphics.setColor(255, 255, 255)
 	end
 end -- end drawEnemies
+
+function drawValues()
+	yPos = 200
+	for i, wave in ipairs(waves) do
+		love.graphics.setColor(0, 0, 255)
+		love.graphics.print("Wave " .. i .. " time: " .. wave.time, 200, yPos)
+		yPos = yPos + 15
+	end
+		
+	yPos = 230	
+	for i, enemy in ipairs(enemies) do
+		love.graphics.print("Enemy  " .. i .. " vals: " .. enemy.position.x .. " " .. enemy.position.y .. " " .. enemy.hp, 200, yPos)
+		yPos = yPos + 15
+	end		
+
+	love.graphics.print("allEnemiesSpawned: " .. allEnemiesSpawned, 50, 50)
+
+	love.graphics.setColor(255, 255, 255)
+end
+
+function newAnimation(image, width, height, duration, x, y, spriteNum)
+    local animation = {}
+    animation.spriteSheet = image;
+    animation.quads = {};
+    animation.x = x
+    animation.y = y
+    animation.maxSprites = spriteNum
+ 
+    for y = 0, image:getHeight() - height, height do
+        for x = 0, image:getWidth() - width, width do
+            table.insert(animation.quads, love.graphics.newQuad(x, y, width, height, image:getDimensions()))
+        end
+    end
+ 
+    animation.duration = duration or 1
+    animation.currentTime = 0
+ 
+    return animation
+end
